daaa2bfcf1c01b7f959cd650bfe013a89f1a86d9
Claudio Cabral <cabral1349@gmail.com>
2017-12-15 11:54:45 +0100

    removed old environ variable

--- srcs/execute/invoke.c
+++ srcs/execute/invoke.c
@@ -6,18 +6,17 @@
 /*   By: claudiocabral <cabral1349@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 10:16:45 by claudioca         #+#    #+#             */
-/*   Updated: 2017/12/10 10:44:24 by claudioca        ###   ########.fr       */
+/*   Updated: 2017/12/15 11:54:07 by claudioca        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <execute.h>
-#include <signal_handlers.h>
-#include <ft_printf.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
-extern char	**environ;
+#include <signal_handlers.h>
+#include <ft_printf.h>
+#include <execute.h>
+#include <environment.h>
 
 void		invoke(char const *command, char **args)
 {
@@ -26,7 +25,7 @@ void		invoke(char const *command, char **args)
 
   pid = fork();
   if (pid == 0) {
-	  if (execve(command, args, environ) == -1) {
+	  if (execve(command, args, get_environment()) == -1) {
 		  ft_dprintf(2, "error in execve: %s not found\n", command);
 		  exit(0);
 	  }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: claudiocabral <cabral1349@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 14:06:17 by claudioca         #+#    #+#             */
-/*   Updated: 2017/12/14 20:08:21 by claudioca        ###   ########.fr       */
+/*   Updated: 2017/12/15 11:46:25 by claudioca        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 #include <shellma.h>
 #include <ft_printf.h>
 #include <hash_table.h>
+#include <environment.h>
 
 int	main(int argc, char **argv) {
+	if (!ft_prepare_env())
+	{
+		ft_dprintf(2, "minishell: failed to setup environment\n");
+		return (1);
+	}
 	if (argc == 1)
 		return (interactive_session());
 	else if (ft_strequ(argv[1],"-c"))
