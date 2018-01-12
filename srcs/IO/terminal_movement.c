0be453e42da454e2ea57aa949fcc7422566d3cfc
Claudio CABRAL <cabral1349@gmail.com>
2018-01-12 18:07:59 +0100

    better behavior for ctrl d

--- srcs/IO/terminal_movement.c
+++ srcs/IO/terminal_movement.c
@@ -6,7 +6,7 @@
 /*   By: claudiocabral <cabral1349@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 09:19:57 by claudioca         #+#    #+#             */
-/*   Updated: 2018/01/12 17:55:18 by ccabral          ###   ########.fr       */
+/*   Updated: 2018/01/12 18:07:36 by ccabral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ int		terminal_eol(t_terminal *terminal, int c)
 int		terminal_move_left(t_terminal *terminal, int c)
 {
 	(void)c;
+	if (terminal->cursor < terminal->prompt_size)
+		terminal->cursor = terminal->prompt_size;
 	if (terminal->cursor == terminal->prompt_size)
 		return (1);
 	terminal_command(MOVE_LEFT, 1);
@@ -47,6 +49,9 @@ int		terminal_move_right(t_terminal *terminal, int c)
 {
 	(void)c;
 	if ((unsigned int)terminal->cursor
+			> terminal->prompt_size + terminal->line->size)
+		terminal->cursor = terminal->prompt_size + terminal->line->size;
+	if ((unsigned int)terminal->cursor
 			== terminal->prompt_size + terminal->line->size)
 		return (1);
 	terminal_command(MOVE_RIGHT, 1);
@@ -59,7 +64,9 @@ int		terminal_move_right(t_terminal *terminal, int c)
 int		terminal_exit(t_terminal *terminal, int c)
 {
 	(void)c;
-	(void)terminal;
-	quit(terminal);
+	if (terminal->line->size == 0)
+		quit(terminal);
+	terminal_move_right(terminal, c);
+	terminal_delete(terminal, c);
 	return (1);
 }
