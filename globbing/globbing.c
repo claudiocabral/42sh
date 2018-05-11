7411b8b39a8e4096c4ab562c94779704cd9aa71c
Clement Trosa <me@trosa.io>
2018-05-11 10:42:50 +0200

    Added milestone

--- globbing/globbing.c
+++ globbing/globbing.c
@@ -6,14 +6,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <globbing.h>
 
 int
 deglob(const char *input)
 {
+	int occur = 0;
+
+	if ((occur = strcspn(input, "*"))) {
 #ifdef DEBUG
-	printf("Deglobbing: \"%s\"\n", input);
+		printf("%s.%d: Got a pattern at %d\n",
+			   __FILE__, __LINE__, occur);
 #endif
+	}
 	return (EXIT_SUCCESS);
 }
--- globbing/globbing.h
+++ globbing/globbing.h
@@ -6,6 +6,9 @@
 #ifndef GLOBBING_H
 #define GLOBBING_H
 
+#define DBG(x) (printf("%s, l%d: ", __FILE__, __LINE__); \
+				x;)
+
 int deglob(const char *input);
 
 #endif /* GLOBBING_H */
