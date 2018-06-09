30fa81584b2be93ba700e72de4817a85e5dc8313
Émile PRÉVOT <user@MacBook-Pro-de-Emile.local>
2018-06-10 00:11:01 +0200

    echo `echo youpi les backtits ça marche°

--- srcs/shellma.c
+++ srcs/shellma.c
@@ -15,13 +15,17 @@
 #include <mysh.h>
 #include <globbing.h>
 #include <ft_printf.h>
+#include <stdio.h>
+
+
+#include "libft.h"
 
 /*
-** Centralized executor
-** @return program status
-*/
+ ** Centralized executor
+ ** @return program status
+ */
 
-int			process_input(char *str)
+int			process_input_after_backtick(char *str)
 {
 	int		return_value;
 	char	*input;
@@ -32,8 +36,107 @@ int			process_input(char *str)
 		free(str);
 		return (ft_printf("42sh: No matchs found.\n"));
 	}
+	input = alias_replace((char *)input);
 	return_value = execute(parse(lex(input)));
 	str ? ft_strdel(&str) : 0;
 	input ? free(input) : 0;
 	return (return_value);
+}
+
+/*
+** process_backtits("echo `echo salut`")
+** return "echo salut"
+*/
+
+char		*ft_strreplace(char *origin, char *piece, size_t pos, size_t len)
+{
+	char *debut;
+	char *millieu;
+	char *fin;
+	char *total;
+
+	debut = ft_memdup(origin, pos);
+//	ft_printf("Début : '%s'\n", debut);
+	millieu = piece;
+//	ft_printf("Millieu : '%s'\n", millieu);
+	fin = ft_strdup(origin + len + 1);
+//	ft_printf("Fin : '%s'\n", fin + 1);
+	total = ft_vjoin(3, debut, millieu, fin);
+	free(debut);
+	free(fin);
+	return (total);
+}
+
+
+void		dellines(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			str[i] = ' ';
+		i++;
+	}
+}
+
+char 		*backtits_replace(char *str, size_t pos)
+{
+	char 	*start;
+	char	*stop;
+	char	*exec;
+	int		p[2];
+	int		out;
+	char    print[1024];
+	char	*now;
+	char	*mem;
+	char 	*oldmem;
+	int 	t;
+
+
+	start = ft_strchr(str + pos, '`');
+	if (!start)
+		return (str);
+	if(!(stop = ft_strchr(start + 1, '`')))
+		return (str); // Error !
+
+	exec = ft_memdup(start + 1, stop - start - 1);
+		out = dup(1);
+	pipe(p);
+	dup2(p[1], 1);
+	close(p[1]);
+	process_input_after_backtick(exec);
+	close(1);
+
+	mem = ft_strdup("");
+	while((t = read(p[0], print, 1023)))
+	{
+		print[t] = '\0';
+		oldmem = mem;
+		mem = ft_vjoin(2, mem, print);
+		free(oldmem);
+	}
+	
+
+	dup2(out, 1);
+	close(p[0]);
+	close(out);
+
+	dellines((char*)mem);
+	
+	
+	now = (ft_strreplace(str, (char*) mem, start - str, stop - str));
+	free(str);
+	now = (backtits_replace(now, (start - str) + ft_strlen(mem)));
+	free(mem);
+	return (now);
+}
+
+
+
+
+int			process_input(char *str)
+{
+	return process_input_after_backtick(backtits_replace(str, 0));
 }
