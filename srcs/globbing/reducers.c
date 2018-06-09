b053ae3a71899b58dfeb329934ecbdc0594b7853
Clement Trosa <me@trosa.io>
2018-06-09 19:07:47 +0200

    Dropper wip [2/2]

--- srcs/globbing/reducers.c
+++ srcs/globbing/reducers.c
@@ -6,7 +6,7 @@
 /*   By: ctrouill <iomonad@riseup.net>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 16:52:06 by ctrouill          #+#    #+#             */
-/*   Updated: 2018/06/09 18:35:46 by ctrouill         ###   ########.fr       */
+/*   Updated: 2018/06/09 19:04:37 by ctrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,37 @@
 
 void		dot_reduce(t_list **head, const char *origin, t_list *aref)
 {
-	aref = *head;
+	t_list	*prev;
 
-	if (*head != NULL && ft_strcmp(origin, "*") == 0)
+	aref = *head;
+	prev = aref;
+	if (*head != NULL && (ft_strcmp(origin, "*") == 0
+						  || ft_strcmp(origin, "**") == 0))
 	{
 		while (aref != NULL)
 		{
 			if (ft_strnequ(aref->content, DOOM, 3))
 			{
-				/* TODO: Poping these elements */
-				dprintf(2, "\" To pop: %s\"\n", aref->content);
+				if (aref == *head)
+				{
+					*head = aref->next;
+					free(aref->content);
+					free(aref);
+					aref = *head;
+				}
+				else
+				{
+					prev->next = aref->next;
+					free(aref->content);
+					free(aref);
+					aref = prev;
+				}
 			}
-			aref = aref->next;
+			else
+			{
+				prev = aref;
+				aref = aref->next;
+			}
 		}
 	}
 }
