6310ae30104ac6403aaa0c84bedf0a5a50444c54
Claudio Cabral <cabral1349@gmail.com>
2018-06-10 18:07:35 +0200

    fix leak

--- srcs/config/loader.c
+++ srcs/config/loader.c
@@ -6,53 +6,55 @@
 /*   By: ctrouill <iomonad@riseup.net>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 18:44:04 by ctrouill          #+#    #+#             */
-/*   Updated: 2018/06/04 03:23:11 by gfloure          ###   ########.fr       */
+/*   Updated: 2018/06/10 18:07:15 by ccabral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mysh.h>
 #include <sys/stat.h>
 
-char			*get_path(char *path)
+char			*get_path(char const *path)
 {
-	int			size;
+	char		*str;
 
+	str = 0;
 	if (path[0] == '~')
 	{
-		size = ft_strlen(path);
-		ft_memmove(path, path + sizeof(char), size);
-		path[size - 1] = 0;
-		path = ft_strjoinfree(ft_getenv_safe("HOME"), path, 'R');
+		str = ft_vjoin(2, ft_getenv_safe("HOME"), path + 1);
 	}
-	return (path);
+	return (str ? str : ft_strdup(path));
 }
 
-char			*rc_loadable(const char *rc)
+int				rc_loadable(const char *rc)
 {
 	struct stat	buf;
 
-	rc = get_path((char *)rc);
 	if (lstat(rc, &buf) == -1)
-		return (NULL);
+		return (0);
 	if (buf.st_mode & S_IFREG)
-		return ((char *)rc);
-	return (NULL);
+		return (1);
+	return (0);
 }
 
 void			load_n_eval(const char *rc)
 {
 	int			fd;
 	char		*line;
+	char		*path;
 
 	line = NULL;
-	if ((rc = rc_loadable(rc)))
+	path = get_path(rc);
+	if (rc_loadable(path))
 	{
 		if ((fd = open(rc, O_RDONLY)) < 0)
+		{
+			free(path);
 			return ;
+		}
 		while (get_next_line(fd, &line) > 0)
 			process_input(line);
 		free(line);
 		close(fd);
 	}
-	rc ? free((char *)rc) : 0;
+	free(path);
 }
